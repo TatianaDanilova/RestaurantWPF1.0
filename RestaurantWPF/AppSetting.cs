e5c2c68ab3a7626434bf9952191bda5cd851a598
Tatiana Danilova <tatianadaniliva2005@gmail.com>
2024-03-24 11:26:53 +0300

    готовый вариант 2.0

---
+++ RestaurantWPF/AppSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantWPF
+{
+    public static class AppSettings
+    {
+        private static int userId;
+
+        public static int UserId
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+    }
+
+}
+
---
+++ RestaurantWPF/Manager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RestaurantWPF
+{
+    internal class Manager
+    {
+        public void AddToOrder(string dish_name, int quantity)
+        {
+            string connectionString = "Data Source=DESKTOP-4HJV1J2;Initial Catalog=RestaurantDB;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand("AddOrderItem", connection))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@dish_name", System.Data.SqlDbType.NVarChar, 100));
+                    cmd.Parameters.Add(new SqlParameter("@quantity", System.Data.SqlDbType.Int));
+                    cmd.Parameters.Add(new SqlParameter("@client_id", System.Data.SqlDbType.Int));
+                    cmd.Parameters["@dish_name"].Value = dish_name;
+                    cmd.Parameters["@quantity"].Value = quantity;
+                    cmd.Parameters["@client_id"].Value = AppSettings.UserId;
+
+                    cmd.ExecuteNonQuery();
+                }
+                try
+                {
+                    MessageBox.Show("Блюдо успешно добавлено в заказ");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}");
+
+                }
+
+            }
+        }
+    }
+}
