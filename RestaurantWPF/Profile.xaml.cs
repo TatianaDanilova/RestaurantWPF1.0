4afb64b20ccb53d3ba2eb0472368d27d01f85e3b
Tatiana Danilova <tatianadaniliva2005@gmail.com>
2024-03-24 15:45:03 +0300

    готовый вариант 2.1

---
+++ RestaurantWPF/Profile.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RestaurantWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Profile.xaml
+    /// </summary>
+    public partial class Profile : Window
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonToMenu(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            menu.Show();
+            Hide();
+            this.Close();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string caption = "";
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+            MessageBoxResult result = MessageBox.Show("Вы уверены в том, что хотите выйти из профиля?", caption, buttons, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+ 
+
+            }
+        }
+    }
+}
